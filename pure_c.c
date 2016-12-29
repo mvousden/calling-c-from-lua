3531b29454acd9b963c717c0ba153293e3310cb7
mvousden <mark.vousden@soton.ac.uk>
2016-12-29 22:25:28 +0000

    Write a norm function.

--- pure_c.c
+++ pure_c.c
@@ -1,7 +1,12 @@
 /* Here is a C source file that defines a C function, and can be compiled and
-   executed independently of Lua. */
+   executed independently of Lua. Compilation:
 
+   gcc -Wall -o pure_c pure_c.c -lm
+*/
+
+#include "math.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int say_hello_from_c()
 {
@@ -9,8 +14,35 @@ int say_hello_from_c()
     return 0;
 }
 
+double norm(double* components, int count)
+/* Simple function to compute and return the 2-norm of an n-dimensional vector
+   with count elements. */
+{
+    int index;
+    double total;
+    for(index=0; index<count; index++)
+    {
+        total += components[index] * components[index];
+    }
+    return pow(total, 0.5);
+}
+
 int main(int argc, char** argv)
 {
+    int count;
+    double* components;
+    double computedNorm;
+
+    count = 5;
+    components = calloc(count, sizeof(double));
+    components[0] = 0.1;
+    components[1] = 0.2;
+    components[2] = 0.3;
+    components[3] = 0.4;
+    components[4] = 0.5;
+    computedNorm = norm(components, count);
+
     say_hello_from_c();
+    printf("Norm of vector: %f\n", computedNorm);
     return 0;
 }
