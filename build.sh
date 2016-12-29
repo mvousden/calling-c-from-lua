a9014d0b3b1e6ea84a784c4e4cb48ede11436096
mvousden <mark.vousden@soton.ac.uk>
2016-12-29 21:44:07 +0000

    Simplify the build script.

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# This Bash script compiles some C to an executable using gcc, and some C to a
-# shared object library for use with Lua.
-gcc -Wall -o pure_c pure_c.c
 gcc -Wall -shared -fPIC -o c_to_lua.so -I/usr/include/lua5.2 -llua5.2 lua_glue.c
