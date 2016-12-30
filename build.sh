fdb1dd817d0ef1848765139d2bdf1c7709bfba78
mvousden <mark.vousden@soton.ac.uk>
2016-12-30 14:34:42 +0000

    Write some glue.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc -Wall -shared -fPIC -o c_to_lua.so -I/usr/include/lua5.2 -llua5.2 lua_glue.c
+gcc -Wall -shared -fPIC -o c_to_lua.so -I/usr/include/lua5.2 -llua5.2 -lm lua_glue.c
--- lua_glue.c
+++ lua_glue.c
@@ -5,14 +5,37 @@
 #include <lauxlib.h>
 #include <lualib.h>
 #include <lua.h>
+#include <stdlib.h>
 
 /* These glue integer functions take a lua_State, and return the number of
-   arguments returned. */
+   values returned. The function lua_pushnumber is used to return values. */
 
 int glue_say_hello_from_c(lua_State* L)
 {
     say_hello_from_c();
     return 0;
+}
+
+int glue_norm(lua_State* L)
+{
+    double returnValue;
+    double* components;
+    int count;
+    int index;
+
+    /* Parse the input array into a form C can understand. */
+    count = lua_gettop(L);
+    components = calloc(count, sizeof(double));
+    for(index=0; index<count; index++)
+    {
+        components[index] = lua_tonumber(L, lua_gettop(L));
+        lua_pop(L, 1);
+    }
+
+    /* Compute the norm, push it, and return. */
+    returnValue = norm(components, count);
+    lua_pushnumber(L, returnValue);
+    return 1;
 }
 
 /* Now we register the functions with Lua. Presumably there's a better way of
@@ -23,5 +46,6 @@ int luaopen_c_to_lua(lua_State* L)
 {
     /* This register function "adds" a function to the library we create. */
     lua_register(L, "say_hello_from_c", glue_say_hello_from_c);
+    lua_register(L, "norm", glue_norm);
     return 0;
 }
