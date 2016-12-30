ed36f8de05e28a7bcf2059467286ade8f97617d2
mvousden <mark.vousden@soton.ac.uk>
2016-12-30 14:40:08 +0000

    Use the function in Lua.

--- pure_lua.lua
+++ pure_lua.lua
@@ -4,4 +4,10 @@
 -- calls a function defined therein.
 
 require("c_to_lua")
+require("io")
+
 say_hello_from_c()
+io.write("The norm of [0.1, 0.2, 0.3, 0.4, 0.5] is " ..
+            norm(0.1, 0.2, 0.3, 0.4, 0.5) .. "\n")
+io.write("The norm of [-1, 0.01, -0.01] is " ..
+            norm(-1, 0.01, -0.01) .. "\n")
